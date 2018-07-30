095ace8e640b7df9a845511e600339d9b31211d3
vikky1322 <41896865+vikky1322@users.noreply.github.com>
2018-07-30 15:00:50 +0530

    Create posneg.java

---
+++ posneg.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n<0)
+		{
+			System.out.println("Negitive");
+		}
+		else if(n>0)
+		{
+			System.out.println("Positive");
+		}
+		else
+		{
+			System.out.println("Zero");
+		}
+		
+	}
+}
