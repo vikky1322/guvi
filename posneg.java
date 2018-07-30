6e28c5fcc3d8b1adc11648483b98141acf40f0d5
vikky1322 <41896865+vikky1322@users.noreply.github.com>
2018-07-30 15:33:52 +0530

    Update posneg.java

--- posneg.java
+++ posneg.java
@@ -2,21 +2,21 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Ideone
+class Posneg
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
-		if(n<0)
-		{
-			System.out.println("Negitive");
-		}
-		else if(n>0)
+		if(n>0)
 		{
 			System.out.println("Positive");
 		}
-		else
+		else if(n<0)
+		{
+			System.out.println("Negitive");
+		}	
+		else if(n==0)
 		{
 			System.out.println("Zero");
 		}
