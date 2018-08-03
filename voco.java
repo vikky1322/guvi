f08e45162849074ef521ccd18d32cfd3fae9bbdd
vikky1322 <41896865+vikky1322@users.noreply.github.com>
2018-08-03 13:57:33 +0530

    Create voco.java

---
+++ voco.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+public static void main (String[] args) throws java.lang.Exception
+{
+
+char ch;
+BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+ch=(char)bf.read();
+if(ch =='a'||ch =='e'||ch =='i'|| ch=='o'||ch =='u')
+System.out.println("Vowel");
+else if(ch=='b'||ch=='c'||ch=='d'||ch=='f'||ch=='g'||ch=='h'||ch=='j'||ch=='k'||ch=='l'||ch=='m'||ch=='n'||ch=='p'||ch=='q'||ch=='r'||ch=='s'||ch=='t'||ch=='v'||ch=='w'||ch=='x'||ch=='y'||ch=='z')	
+System.out.println("Consonant");
+else
+System.out.println("invalid");
+
+
+}
+}
