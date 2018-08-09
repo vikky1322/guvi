95b0b26a855c63815a8e6551722618180f19d9d1
vikky1322 <41896865+vikky1322@users.noreply.github.com>
2018-08-09 11:25:06 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n%4==0)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+}
